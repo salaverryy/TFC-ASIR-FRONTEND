40bd571d92cd7e60293d5c2a585628d43a1cf2c1
salaverry <andres.2005.fut@gmail.com>
2025-06-11 23:28:22 +0100

    decodifica el token para recuperar el grupo al que pertenece el usuario

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,18 +1,28 @@
 // src/services/authService.js
+import { jwtDecode } from 'jwt-decode';
 import api from './api'; // Asegúrate de que esto apunte a axios con baseURL: http://localhost:8080
 
 const login = async (email, password) => {
   try {
     const response = await api.post('/auth/login', { email, password });
 
-    const { accessToken, role } = response.data;
+    const { accessToken } = response.data;
 
-    if (!accessToken || !role) {
-      throw new Error('Respuesta inválida del servidor');
+    if (!accessToken) {
+      throw new Error('Token no recibido del servidor');
     }
 
+    // Decodificar el token JWT
+    const decodedToken = jwtDecode(accessToken);
+
+    // Extraer el rol desde cognito:groups
+    const role = decodedToken['cognito:groups']?.[0] ?? 'UNKNOWN';
+
+    // Guardar en localStorage
     localStorage.setItem('token', accessToken);
     localStorage.setItem('role', role);
+
+    console.log(localStorage.getItem('role'));
 
     return { success: true, token: accessToken };
   } catch (error) {
